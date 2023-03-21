61fa38be8fc206e707f6f5168f9be2506f10e2de
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-03-21 12:04:24 -0300

    adicionando border radius no header

--- lib/components/sections/header.dart
+++ lib/components/sections/header.dart
@@ -5,8 +5,21 @@ class Header extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // ignore: avoid_unnecessary_containers
     return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: <Color>[
+            Color.fromRGBO(103, 99, 234, 1.0),
+            Color.fromRGBO(155, 105, 254, 1.0),
+            Color.fromRGBO(195, 107, 255, 1.0)
+          ],
+        ),
+        borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(10),
+        ),
+      ),
       child: Padding(
         padding: const EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 16.0),
         child: Row(
