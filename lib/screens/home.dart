1390becfae3fadfd71242bc6ce392069e5fffa93
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-04-13 17:24:16 -0300

    produzindo o contentDivision

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,6 @@
 import 'package:alubank/components/box_card.dart';
-import 'package:alubank/components/color_dot.dart';
+import 'package:alubank/components/content_division.dart';
 import 'package:alubank/components/sections/header.dart';
-import 'package:alubank/themes/theme_colors.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -11,10 +10,10 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: <Widget>[
-          const Header(),
+        children: const <Widget>[
+          Header(),
           BoxCard(
-            boxContent: ColorDot(color: ThemeColors.recentActivity['spent']),
+            boxContent: ContentDivision(),
           ),
         ],
       ),
