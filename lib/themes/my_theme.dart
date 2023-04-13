51b6a90be1bbc5431d88b28885cea9d3d35e03ef
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-04-13 15:39:59 -0300

    criando um tema para a aplicacao

---
+++ lib/themes/my_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// ignore: non_constant_identifier_names
+ThemeData MyTheme = ThemeData(
+    primarySwatch: Colors.purple,
+    primaryColor: Colors.purple,
+    brightness: Brightness.dark);
